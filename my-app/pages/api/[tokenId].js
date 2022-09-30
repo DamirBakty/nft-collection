c81902634bbd984106ad55bf84acca073b9d3dfe
Damir Bakty <damir050602@gmail.com>
2022-09-30 17:01:03 +0600

    OWN PHOTO

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
     const tokenId = req.query.tokenId;
     // As all the images are uploaded on github, we can extract the images from github directly.
     const image_url =
-      "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+      "https://raw.githubusercontent.com/DamirBakty/nft-collection/main/my-app/public/own.jpg";
     // The api is sending back metadata for a Crypto Dev
     // To make our collection compatible with Opensea, we need to follow some Metadata standards
     // when sending back the response from the api
@@ -11,6 +11,6 @@ export default function handler(req, res) {
     res.status(200).json({
       name: "Damir #" + tokenId,
       description: "Crypto Dev is a collection of developers in crypto",
-      image: image_url + tokenId + ".svg",
+      image: image_url + tokenId,
     });
   }
